0dfd68dd17d7c3aca81e65924eedf21a09dd0322
Yuri Numerov <achi@hush.ai>
2015-04-24 17:24:12 +0200

    line, pixel and square functions

--- engine.py
+++ engine.py
@@ -28,31 +28,39 @@ class context:
     os.system('clear')
     for i in self.grid: print "".join(i)
 
-  def line(self,x1,y1,x2,y2,char):
+  def line(self,x0,y0,x1,y1,char):
     """
-    Draws a line from (x1,y1) to (x2,y2)
-    """
+    Stole this shit from wikipedia
 
-    xdiff=abs(x2-x1)
-    ydiff=abs(y2-y1)
-
-    if xdiff>ydiff:
-      if x2>x1:
-        for i in range(xdiff):
-      else:
-        for i in range(xdiff):
-    else:
-      if y2>y1:
-        for i in range(ydiff):
-      else:
-        for i in range(ydiff):
-
-  def rectangle(self,x,y,xsize,ysize):
-    """
-    Draws a rectangle in (x,y)
+    https://en.wikipedia.org/wiki/Bresenham's_line_algorithm#Line_equation
     """
 
-    pass
+    sign=lambda x: cmp(x,0)
+    deltax=x1-x0
+    deltay=y1-y0
+    error=0
+    try:
+      deltaerr=abs(deltay/deltax)  #Assume deltax != 0 (line is not vertical),
+      y=y0
+      for x in range(x0,x1+1):
+        self.grid[y][x]=char
+        error+=deltaerr
+        while error>=0.5:
+          self.grid[y][x]=char
+          y+=sign(y1-y0)
+          error-=1
+    except ZeroDivisionError:
+      for i in range(abs(deltay)): self.grid[y0+(i*sign(y1-y0))][x0]=char
+
+  def putpixel(self,x,y,char):
+    self.grid[y][x]=char
+
+  def rectangle(self,x,y,xsize,ysize,char):
+
+    self.line(x,y,x+xsize,y,char)
+    self.line(x,y,x,y+ysize,char)
+    self.line(x+xsize,y,x+xsize,y+ysize,char)
+    self.line(x,y+ysize,x+ysize,y+ysize,char)
 
 if __name__=="__main__":
   pass
